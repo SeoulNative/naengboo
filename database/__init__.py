6312df92c8f7e88b6b383cb9d0b9078fb2b482fb
coloryourlife <95chris0319@gmail.com>
2021-04-29 18:12:13 +0900

    init database

---
+++ database/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+from .db import initialize_db
+
+app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': "naengboo",
+    'host': '3.36.96.53',
+    'port': 27017,
+    'username': 'theseoulnative',
+    'password': 'tjdnf0401!'
+}
+db = MongoEngine(app)
