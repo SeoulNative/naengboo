8d0ce3ab9c33e5b627a0189c7c16744613ee8e06
coloryourlife <95chris0319@gmail.com>
2021-05-05 15:20:07 +0900

    add recipes model

--- database/models.py
+++ database/models.py
@@ -11,3 +11,18 @@ class Refrigerator(db.Document):
     rfg_name = db.StringField(required=True)
     rfg_img_url = db.URLField(required=True)
     rfg_memo = db.StringField(required=True)
+
+
+class Recipes(db.Document):
+
+    class RecipeIngredients(db.EmbeddedDocument):
+        ingred_id = db.ObjectIdField(required=True)
+        name = db.StringField(required=True)
+        quantity = db.StringField(required=True)
+
+    food_name = db.StringField(required=True)
+    main_ingredients = db.EmbeddedDocumentListField(RecipeIngredients, required=True)
+    sub_ingredients = db.EmbeddedDocumentListField(RecipeIngredients, required=True)
+    recipe_steps = db.DictField(required=True)
+    hash_tags = db.DictField(required=True)
+
