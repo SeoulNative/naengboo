d47d9af62006cd8cdf2bd897a8163c09e54a4017
Gyeongmin Ha <gaonrudal@gmail.com>
2021-05-21 18:21:25 +0900

    Add db connection test api

--- app/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from flask_restx import Api
--- app/views/refrigerators.py
+++ app/views/refrigerators.py
@@ -1,6 +1,21 @@
-from flask_restx import Namespace
+from flask_restx import Namespace, Resource
+from app import db
+
 
 Refrigerators = Namespace(
     name="Refrigerators",
     description="APIs for refrigerators"
 )
+
+
+# TODO(livlikwav): fix this with dao & model
+# test db connection
+@Refrigerators.route('/ingredients')
+class Ingredients(Resource):
+    def post(self):
+        test = db['test']
+        test.insert_one({'username' : 'db-test','msg' : 'wow it works'})
+
+        return {
+            "result" : "success"
+        }
